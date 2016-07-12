a50ba5905f53af8d63fa36e59fadbafd26dbf9c6
desairavi1101 <desai.ravi1101@gmail.com>
2016-07-12 21:51:29 +0530

    Adds Remaining methods

--- TrainApplicationLibrary/Classes/Reservation.cs
+++ TrainApplicationLibrary/Classes/Reservation.cs
@@ -9,7 +9,7 @@ namespace TrainApplicationLibrary.Classes
     public class Reservation : IReservation
     {
         private static int NextTrainId = 1;
-        private static int NextTicketId = 1;
+        private static int NextTicketNo = 1;
 
         private List<ITrain> Trains;
         private List<ITicket> Tickets;
@@ -51,10 +51,80 @@ namespace TrainApplicationLibrary.Classes
 
         }
 
+        public int DeleteTrain(int trainId)
+        {
+            var ticket = (from t in Tickets.OfType<Ticket>()
+                          where t.TrainId == trainId
+                          select t).FirstOrDefault();
 
+            if (ticket == default(Ticket))
+            {
+                return 0;
+            }
+            Trains.OfType<Train>().ToList().RemoveAll(t => t.Id == trainId);
+            return 1;
 
+        }
 
+        public List<ITicket> ViewAllBookedTickets()
+        {
+            return Tickets;
+        }
 
+        public int BookTicket(int noOfPassenger, int trainId, out int totalPrice)
+        {
+            if (trainId == 0)
+            {
+                totalPrice = 0;
+                return 0;
+            }
+            int ticketNo = NextTicketNo++;
+            ITicket ticket = new Ticket()
+            {
+                TicketNo = ticketNo,
+                TrainId = trainId,
+                NoOfPassenger = noOfPassenger
+            };
+
+            Tickets.Add(ticket);
+            totalPrice = 200 * noOfPassenger;
+            return ticketNo;
+        }
+
+        public int EditTrain(int trainId, string source, string destination)
+        {
+            var train = (from t in Trains.OfType<Train>()
+                         where t.Id == trainId
+                         select t).FirstOrDefault();
+
+            if (train == default(Train))
+            {
+                return 0 ;
+            }
+
+            train.Source = source;
+            train.Destination = destination;
+
+            return 1;        
+        }
+
+        public int EditTrain(int trainId, string name, string source, string destination)
+        {
+            var train = (from t in Trains.OfType<Train>()
+                         where t.Id == trainId
+                         select t).FirstOrDefault();
+
+            if (train == default(Train))
+            {
+                return 0;
+            }
+
+            train.Source = source;
+            train.Destination = destination;
+            train.Name = name;
+
+            return 1;
+        }
 
     }
 }
